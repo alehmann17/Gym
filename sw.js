810b8c92cebefd14db1468bc568cfd004c7cbfde
alehmann17 <aclehman31@yahoo.com>
2026-08-12 18:25:05 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "lb24-v3";
+const CACHE = "lb24-v4";
 const ASSETS = ["./", "./index.html", "./app.js", "./manifest.json", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", (e) => {
